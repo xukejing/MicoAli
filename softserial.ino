5331684b8cdce16db4c55a89fe11c92595736542
xukejing <648460636@qq.com>
2018-10-18 03:10:49 +0800

    Add files via upload

---
+++ softserial.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+int mark=0;
+void setup() {
+  // put your setup code here, to run once:
+mySerial.begin(115200);
+Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0)
+  {
+    Serial.read();
+    mark = 1;
+  }
+if(mark==1)
+{
+  mark=0;
+  mySerial.print("AT+TTS=发现猫咪\r");
+}
+}
